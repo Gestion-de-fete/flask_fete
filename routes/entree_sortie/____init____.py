337d5857c0af1416cde6b8b92e95c8fa7965a593
Samuel255231 <rakotonandrasanasamuel5@gmail.com>
2025-05-26 12:36:54 +0300

    vide

--- routes/entree_sortie/____init____.py
+++ routes/entree_sortie/____init____.py
@@ -3,6 +3,8 @@ from .update_entree import update_entree
 from .update_sortie import update_sortie
 from .get_etat import get_etat
 from .get_entree_sortie import get_entree_sortie
+from .dashboard import get_dashboard_data
+from .delete_entree_sortie import delete_entree_sortie
 
 # Création du blueprint avec un préfixe d'URL
 entree_sortie_bp = Blueprint('entree_sortie', __name__, url_prefix='/api/entree_sortie')
@@ -12,4 +14,8 @@ entree_sortie_bp.route('/entree', methods=['PUT'])(update_entree)
 entree_sortie_bp.route('/sortie', methods=['PUT'])(update_sortie)
 entree_sortie_bp.route('/etat', methods=['GET'])(get_etat)
 entree_sortie_bp.route('/fetch', methods=['GET'])(get_entree_sortie)
+entree_sortie_bp.route('/number', methods=['GET'])(get_dashboard_data)
+entree_sortie_bp.route('/delete', methods=['DELETE'])(delete_entree_sortie)
+
+
 
